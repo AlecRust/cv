36124c8b09dcfa52c33fbe4ace2c561afcab4713
Alec Rust <me@alecrust.com>
2024-05-03 21:54:10 +0100

    Update dependency eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const js = require('@eslint/js')
+const eslintConfigPrettier = require('eslint-config-prettier')
+const globals = require('globals')
+
+module.exports = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
+]
